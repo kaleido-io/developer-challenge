704fb64c6f4a03dc6977120c9a0fc045abc3c30e
David Echelberger <eberger727@gmail.com>
2021-09-07 19:23:12 -0400

    remove-mocks fixing unit test amplify config

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
