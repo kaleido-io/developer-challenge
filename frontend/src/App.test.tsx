704e3285f1b255141f2b14f61b8af71747dd013b
David Echelberger <eberger727@gmail.com>
2021-09-08 00:44:03 -0400

    remove-mocks testing config

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Amplify from "aws-amplify";
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
+Amplify.configure({});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
