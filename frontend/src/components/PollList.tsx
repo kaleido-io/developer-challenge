0e251b497d175c2bb814c03f979acbcf0c80b1eb
Nick Miano <nick@narrativ.ai>
2024-06-07 12:41:27 -0400

    don't show polls if logged out

--- frontend/src/components/PollList.tsx
+++ frontend/src/components/PollList.tsx
@@ -15,12 +15,20 @@ const PollList = () => {
         setPolls(response);
       }
     };
-    fetchPolls();
+    const token = localStorage.getItem('token');
+    if (token) {
+      fetchPolls();
+    }
+
   }, []);
 
   return (
     <div className="mt-8">
-      <h2 className="text-xl font-bold mb-4">Available Polls</h2>
+      {localStorage.getItem('token')
+        ? <h2 className="text-xl font-bold mb-4">Available Polls</h2>
+        : <h1 className="text-xl font-bold mb-4">To create a poll or vote in a poll, please log in or create an account!</h1>
+      }
+
       {message && <p className="mt-4">{message}</p>}
 
       {polls.map((poll) => (
